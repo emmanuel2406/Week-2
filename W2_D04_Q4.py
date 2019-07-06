7039f8993acdf6d69651c206206ef280ac9da88e
emmanuel2406 <51539286+emmanuel2406@users.noreply.github.com>
2019-07-06 05:13:56 -0400

    solved wdmatch

--- W2_D04_Q4.py
+++ W2_D04_Q4.py
@@ -1,12 +1,16 @@
-word = input('INPUT?')
-search_code = list(input())
-output = '' 
-for char in word:
-    for code in search_code:
-        search_code.remove(code)
-        if char == code:
-            output += code
-            break
-       
-if output == word:
+word = input('INPUT?\n')
+search_code = input()
+count = 0
+index = 0 
+if word[0] not in search_code:
+    count = 1
+else:
+    for char in word:
+        index = search_code.find(char,index)
+        if index == -1:
+            count = 1
+
+if count == 1:
+    print(' ')
+else:
     print(word)
